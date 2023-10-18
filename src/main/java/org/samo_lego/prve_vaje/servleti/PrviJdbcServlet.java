635f4e701f077dbfd844eadfa6e7d94be4467ee4
samolego <34912839+samolego@users.noreply.github.com>
2023-10-18 15:16:14 +0200

    Dokoncane 1. vaje

--- src/main/java/org/samo_lego/prve_vaje/servleti/PrviJdbcServlet.java
+++ src/main/java/org/samo_lego/prve_vaje/servleti/PrviJdbcServlet.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 @WebServlet("/servlet")
 public class PrviJdbcServlet extends HttpServlet {
@@ -23,27 +24,44 @@ public class PrviJdbcServlet extends HttpServlet {
 
         PrintWriter writer = resp.getWriter();
 
-        System.out.println("Moj prvi servlet.");
+        //System.out.println("Moj prvi servlet.");
 
         //izpis konfiguracije mikrostoritev
         Optional<String> microserviceName = ConfigurationUtil.getInstance().get("kumuluzee.name");
         microserviceName.ifPresent(s -> writer.println("Izpis generiran v mikrostoritvi: " + s + "\n"));
 
         // dostop do podatkovne baye
-        BaseDao uporabnikDao = UporabnikDaoImpl.getInstance();
+        BaseDao<Uporabnik> uporabnikDao = UporabnikDaoImpl.getInstance();
 
         Uporabnik uporabnik = new Uporabnik("Miha", "Novak", "mihanovak");
 
         // dodajanje uporabnika
-        //writer.append("Dodajam uporabnika:\n" + uporabnik.toString());
-        //uporabnikDao.vstavi(uporabnik);
-        //writer.append("\n\n");
+        /*writer.append("Dodajam uporabnika:\n").append(uporabnik.toString());
+        uporabnikDao.vstavi(uporabnik);*/
+        List<Uporabnik> uporabniki2 = uporabnikDao.vrniVse();
+        writer.append("\n\nSeznam obstojecih uporabnikov (len= ").append(String.valueOf(uporabniki2.size())).append("):\n");
+        uporabniki2.forEach(u -> writer.append(u.toString()).append("\n"));
+        writer.append("\n\n");
 
         // demonstracija preostalih metod
+        final var posodobljen = uporabnikDao.vrni(2);
+        posodobljen.ifPresent(up -> {
+            up.setIme("Janez" + (char) ('A' + new Random().nextInt(24)));
+            uporabnikDao.posodobi(up);
+            writer.append("Uporabnik ").append(up.getIme()).append(" posodobljen!");
+
+            List<Uporabnik> uporabniki1 = uporabnikDao.vrniVse();
+            writer.append("\n\nSeznam obstojecih uporabnikov (len= ").append(String.valueOf(uporabniki1.size())).append("):\n");
+            uporabniki1.forEach(u -> writer.append(u.toString()).append("\n"));
+
+            // Izbris
+            //uporabnikDao.odstrani(up.getId());
+        });
+
 
         // izpis vseh uporabnikov
-        writer.append("Seznam obstojecih uporabnikov:\n");
         List<Uporabnik> uporabniki = uporabnikDao.vrniVse();
+        writer.append("\n\nSeznam obstojecih uporabnikov (len= ").append(String.valueOf(uporabniki.size())).append("):\n");
         uporabniki.forEach(u -> writer.append(u.toString()).append("\n"));
     }
 }
